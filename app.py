6f4a38317043c05dcac8f9baddec0374b1c45a16
Abigail :] <powheart26@proton.me>
2024-11-08 19:39:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="api/html")
 
 @app.route("/")
 def index():
-    return render_template("api/html/home.html")
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
